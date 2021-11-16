e45611533dfc220b9c713ced0605cb47898f1a6b
Erik Iverson <erik.iverson@drake.edu>
2021-11-16 16:13:11 -0600

    added routes and pages

--- genetic-algorithms/src/app/app.component.ts
+++ genetic-algorithms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ControlService } from 'src/libs/services/control.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  constructor(
+    public controlService: ControlService,
+  ) { }
 }
